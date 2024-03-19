105762ce1fc5518389f41098da27c30530449852
Saba Chitorelidze <chitorelidze.99@gmail.com>
2024-03-20 02:40:39 +0400

    working

--- script/script.js
+++ script/script.js
@@ -3,3 +3,16 @@ let capitals = ["Tbilisi", "Rome", "Madrid", "Berlin"];
 let result = Array.isArray(capitals);
 
 console.log(result);
+
+let firstElement = capitals[0];
+console.log(firstElement);
+console.log(capitals.at(-1));
+
+let color = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "yellow"];
+let o = ["th", "st", "nd", "rd"];
+
+console.log("1" + o[1] + " choice is " + color[0]);
+
+console.log("2" + o[2] + " choice is " + color[1]);
+
+console.log("3" + o[3] + " choice is " + color[2]);
