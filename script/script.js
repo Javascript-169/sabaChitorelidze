3aba03e50d044803b6dd2e5e2246915853c157db
Saba Chitorelidze <163816724+saba2899@users.noreply.github.com>
2024-03-23 17:33:08 +0400

    working

--- script/script.js
+++ script/script.js
@@ -1,37 +1,6 @@
-let capitals = ["Tbilisi", "Rome", "Madrid", "Berlin"];
+function square(x) {
+    let z = x * x;
+    document.write(x + " კვადრატში ტოლია " + z);
+}
 
-let result = Array.isArray(capitals);
-
-console.log(result);
-
-let firstElement = capitals[0];
-console.log(firstElement);
-console.log(capitals.at(-1));
-
-let color = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "yellow"];
-let o = ["th", "st", "nd", "rd"];
-
-console.log("1" + o[1] + " choice is " + color[0]);
-
-console.log("2" + o[2] + " choice is " + color[1]);
-
-console.log("3" + o[3] + " choice is " + color[2]);
-
-
-let stundet = {
-    sakheli: "Giorgi",
-    gvari: "Chitorelidze",
-    profesia: "Fraud specialist",
-    misamarti: "Tbilisi"
-};
-console.log(stundet);
-
-stundet.age = 24;
-
-delete stundet.profesia
-
-stundet.sakheli = "Saba";
-
-console.log(stundet);
-
-
+square(4);
