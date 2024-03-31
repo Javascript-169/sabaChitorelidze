49dbc8947afafbf30c452f3be588283dc20695ae
Saba Chitorelidze <chitorelidze.99@gmail.com>
2024-03-31 14:57:53 +0400

    working

---
+++ script/homework5.js
@@ -0,0 +1,7 @@
+// შექმენით ფუნქცია,მაგ. სახელით: factorialCalculator(number), რომელიც დაიანგარიშებს და ეკრანზე გამოიტანს მისთვის პარამეტრის სახით მიწოდებული რიცხვის ფაქტორიალს, მაგ. 5! => 2*3*4*5 = 120, 
+
+const factorial = function fac(x) {
+    return x < 2 ? 1 : x * fac(x - 1);
+};
+
+console.log(factorial(5));
