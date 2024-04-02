ce4e1c8f4e0e302c1a19812b010dafa73c4b9d75
Saba Chitorelidze <chitorelidze.99@gmail.com>
2024-04-03 01:32:12 +0400

    working

--- script/homework6.js
+++
@@ -1,8 +0,0 @@
-let word = prompt("Enter a word");
-
-function firstLetter() {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-};
-
-
-alert(firstLetter());
---
+++ script/task1.js
@@ -0,0 +1,6 @@
+let people = ["Saba", "Luka", "Tako", "Keti", "Lali", "Lela", "Giorgi", "Lika","Nanuka", "Levani"];
+
+for (let i = 2; i < people.length; i += 3) {
+    console.log(people[i]);
+}
+
