a94d94e4dea5f1ae2f2c222a295002562d5eabb5
SabaChitorelidze <chitorelidze.99@gmail.com>
2024-04-06 22:30:57 +0400

    working

--- script/task6.js
+++
@@ -1,6 +0,0 @@
-// შექმენით ფუნქცია, რომელიც მასზე მიწოდებულ სტრიქონს გადააქცევს მასივად. მაგ. stringToArray(testString) => თუ მივაწვდით სტრიქონს “Hello, World” => გამოიტანს მის ვერსიას მასივში. [“Hello”, “World”]st
-
-let text = "Hello World!"
-const myArray = text.split (" ");
-
-document.getElementById("demo").innerHTML = myArray[0];
