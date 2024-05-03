730786bcf154d2a7ae625c28cb556b652747df56
SabaChitorelidze <chitorelidze.99@gmail.com>
2024-05-03 21:41:40 +0400

    working

--- script/menu.js
+++ script/menu.js
@@ -1,2 +1,21 @@
 let homeMenu = document.querySelector(".homeMenu");
 let main = document.querySelector(".main");
+
+
+const divs = document.querySelectorAll('.active');
+
+divs.forEach(el => {
+  el.addEventListener('click', () => {
+    divs.forEach(otherDiv => {
+      if (otherDiv !== el) {
+        otherDiv.querySelectorAll('i, span').forEach(child => {
+          child.classList.remove('blue');
+        });
+      }
+    });
+    el.querySelectorAll('i, span').forEach(child => {
+      child.classList.toggle('blue');
+    });
+  });
+  console.log();
+});
