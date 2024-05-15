7a7ba0d788ca0e89900e12139562a33c51cba41f
SabaChitorelidze <chitorelidze.99@gmail.com>
2024-05-15 20:34:50 +0400

    working

--- script/menu.js
+++ script/menu.js
@@ -1,5 +1,13 @@
 const windowPathname = window.location.pathname;
 const navLinks = document.querySelectorAll('.navMenu a');
+const authorization = document.querySelector(`.authorization`);
+const signIn = document.querySelector(`.signIn`);
+const close = document.getElementById("close");
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay')
+
 
 navLinks.forEach(navLinks => {
   const navLinkPathname = new URL(navLinks.href).pathname;
@@ -11,3 +19,35 @@ navLinks.forEach(navLinks => {
 });
 
 
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.activeS')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModalModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal === null) return
+  modal.classList.add('activeS')
+  overlay.classList.add('activeS')
+}
+
+function closeModal(modal) {
+  if (modal === null) return
+  modal.classList.remove('activeS')
+  overlay.classList.remove('activeS')
+}
